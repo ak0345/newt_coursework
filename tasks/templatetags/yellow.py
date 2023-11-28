ed3c6b37f24e9e6bd1a62887c72907d460df31bd
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2023-11-28 21:04:10 +0000

    Tidy up visuals on search and dashboard

--- tasks/templatetags/yellow.py
+++ tasks/templatetags/yellow.py
@@ -9,9 +9,15 @@ register = template.Library()
 def yellow(text, value):
     if text is not None:
         text = str(text)
-        src_str = re.compile(value, re.IGNORECASE)
-        str_replaced = src_str.sub(f"<span class=\"yellow\">{value}</span>", text)
+        if text.istitle():
+            src_str = re.compile(value)
+            str_replaced = src_str.sub(
+                f'<span class="yellow">{value.capitalize()}</span>', text
+            )
+        else:
+            src_str = re.compile(value)
+            str_replaced = src_str.sub(f'<span class="yellow">{value}</span>', text)
     else:
-        str_replaced = ''
+        str_replaced = ""
 
     return mark_safe(str_replaced)
