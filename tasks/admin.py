428db2931670049c4863e0d7ef48ee4c8f88641e
Yusuf Yusuf <yusuf.1.yusuf@kcl.ac.uk>
2023-12-03 19:30:50 +0000

    Add notifications for users requesting to join team

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,22 +1,48 @@
 from django.contrib import admin
-from .models import User, Task, Team
+from .models import User, Task, Team, Invitation
 
 # Register your models here.
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = [
-        'username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff',
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_staff",
     ]
+
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = [
-        'unique_identifier', 'team_name','description', 'team_owner','creation_date','last_modified',
+        "unique_identifier",
+        "team_name",
+        "description",
+        "team_owner",
+        "creation_date",
+        "last_modified",
     ]
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = [
-        'id','task_heading','creation_date', 'last_modified','task_owner','task_complete',
+        "id",
+        "task_heading",
+        "creation_date",
+        "last_modified",
+        "task_owner",
+        "task_complete",
+    ]
+
+
+@admin.register(Invitation)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "team_to_join",
+        "user_requesting_to_join",
     ]
